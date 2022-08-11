7d84ac399caa8effea1c89142ba72f9bde095850
Eugen <ezdiviner@gmail.com>
2022-08-11 19:01:54 +0300

    Finally update Task4

--- Homework1/src/Homework1_Task4/Computer.java
+++ Homework1/src/Homework1_Task4/Computer.java
@@ -1,10 +1,20 @@
 package Homework1_Task4;
 
 public class Computer {
-    String name;
+    int name;
 
     public static void main(String[] args) {
-        
+        Computer[] computers = new Computer[5];
+        computers[0] = new Computer();
+        computers[1] = new Computer();
+        computers[2] = new Computer();
+        computers[3] = new Computer();
+        computers[4] = new Computer();
+
+        for (int i = 0; i < computers.length; i++) {
+            computers[i].name = i+1;
+            System.out.println("compputers: " + computers[i].name);
+        }
     }
 }
 
