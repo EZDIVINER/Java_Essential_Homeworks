745082932dd2e56b813a8400253709b3ac1fb5fa
Eugen <ezdiviner@gmail.com>
2022-08-11 14:12:40 +0300

    Added 2 field and 2 methods in class Rectangle

--- Homework1/src/Homework1_Task2/Rectangle.java
+++ Homework1/src/Homework1_Task2/Rectangle.java
@@ -1,4 +1,13 @@
 package Homework1_Task2;
 
 public class Rectangle {
+    double side1;
+    double side2;
+
+    double areaCalculator (double side1, double side2){
+        return side1*side2;
+    }
+    double perimeterCalculator (double side1, double side2){
+        return 2 * side1 + 2 * side2;
+    }
 }
