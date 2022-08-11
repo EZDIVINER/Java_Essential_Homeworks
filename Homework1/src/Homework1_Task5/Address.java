65017f038f7e7c44fa3273eb6e20b78a58c81864
Eugen <ezdiviner@gmail.com>
2022-08-11 19:28:02 +0300

    Added a new methods for class Address

--- Homework1/src/Homework1_Task5/Address.java
+++ Homework1/src/Homework1_Task5/Address.java
@@ -19,6 +19,51 @@ public class Address {
         this.house = house;
         this.apartment = apartment;
     }
-    
+    public int getIndex() {
+        return index;
+    }
+    public int setIndex(int index){
+        this.index = index;
+        return index;
+    }
+
+    public String getCountry(){
+        return country;
+    }
+    public String setCountry(String country) {
+        this.country = country;
+        return country;
+    }
+
+    public String getCity(){
+        return city;
+    }
+    public String setCity(String city) {
+        this.city = city;
+        return city;
+    }
+
+    public String getStreet(){
+        return street;
+    }
+    public String setStreet(String street) {
+        this.street = street;
+        return street;
+    }
+
+    public int getHouse(){
+        return house;
+    }
+    public int setHouse(int house){
+        this.house = house;
+        return house;
+    }
+
+    public int getApartment(){
+        return apartment;
+    }
+    public void setApartment(int apartment){
+        this.apartment = apartment;
+    }
 
 }
