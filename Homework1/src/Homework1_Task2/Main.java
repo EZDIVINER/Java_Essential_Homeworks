e44c6832d8f470018aaa431148817e2cbd256898
Eugen <ezdiviner@gmail.com>
2022-08-11 14:26:42 +0300

    Update class Main

--- Homework1/src/Homework1_Task2/Main.java
+++ Homework1/src/Homework1_Task2/Main.java
@@ -1,8 +1,20 @@
 package Homework1_Task2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task2");
-        
+        Scanner in = new Scanner(System.in);
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Введите длину первой стороны");
+        rectangle.side1 = in.nextDouble();
+        System.out.println("Введите длину второй стороны");
+        rectangle.side2 = in.nextDouble();
+
+        System.out.println("Площадь прямоугольника: " + rectangle.areaCalculator(rectangle.side1, rectangle.side2));
+        System.out.println("Периметр прямоугольника : " + rectangle.perimeterCalculator(rectangle.side1, rectangle.side2));
+
     }
 }
