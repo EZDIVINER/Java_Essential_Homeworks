07d50bb9266e64f1b70bd659e390436e5ac08216
Eugen <ezdiviner@gmail.com>
2022-08-11 16:19:58 +0300

    Update Title

--- Homework1/src/Homework1_Task3/Title.java
+++ Homework1/src/Homework1_Task3/Title.java
@@ -3,6 +3,6 @@ package Homework1_Task3;
 public class Title {
     String title;
     void show (){
-        System.out.println("Содержание книги: " + title);
+        System.out.println("Название книги: " + title);
     }
 }
