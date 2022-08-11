946908bb323796ed870be7a04ed0a78b81d58445
Eugen <ezdiviner@gmail.com>
2022-08-11 19:11:29 +0300

    Initial new class Adress in Task5

---
+++ Homework1/src/Homework1_Task5/Address.java
@@ -0,0 +1,24 @@
+package Homework1_Task5;
+
+public class Address {
+    int index;
+    String country;
+    String city;
+    String street;
+    int house;
+    int apartment;
+
+    public Address(){
+
+    }
+    public Address(int index, String country, String city, String street, int house, int apartment){
+        this.index = index;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+    }
+    
+
+}
