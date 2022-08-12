0ef1712ee90c75a20e8aca0e7099a77654e47458
Eugen <ezdiviner@gmail.com>
2022-08-12 18:07:44 +0300

    Added new class Car

--- Homework2/src/Homework2_Task2/Car.java
+++ Homework2/src/Homework2_Task2/Car.java
@@ -1,4 +1,18 @@
 package Homework2_Task2;
 
 public class Car {
+    int year;
+    String color;
+//    Конструктор по умолчанию
+    public Car(){
+
+    }
+//    Конструктор с одним параметром
+    public Car(int year){
+
+    }
+//    Конструктор с 2 параметрами
+    public Car(int year, String color){
+        
+    }
 }
