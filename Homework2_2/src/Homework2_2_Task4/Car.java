7fbc7d60f82178779e87e5bf605b542a9c5d26bb
Eugen <ezdiviner@gmail.com>
2022-08-17 14:49:19 +0300

    First update class Car

--- Homework2_2/src/Homework2_2_Task4/Car.java
+++ Homework2_2/src/Homework2_2_Task4/Car.java
@@ -1,4 +1,40 @@
 package Homework2_2_Task4;
 
 public class Car {
+    int year;
+    double speed;
+    int weight;
+    String color;
+
+    public Car(){
+        this.year = 2021;
+        this.speed = 304.2;
+        this.weight = 15233;
+        this.color = "Red";
+    }
+    public Car(int year){
+        this.year = year;
+        this.speed = 304.2;
+        this.weight = 15233;
+        this.color = "Red";
+    }
+    public Car(int year, double speed){
+        this.year = year;
+        this.speed = speed;
+        this.weight = 1233;
+        this.color = "Green";
+    }
+    public Car (int year, double speed, int weight){
+        this.year = year;
+        this.speed = speed;
+        this.weight = weight;
+        this.color = "Red";
+    }
+    public Car (int year, double speed, int weight, String color){
+        this.year = year;
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+    }
+
 }
