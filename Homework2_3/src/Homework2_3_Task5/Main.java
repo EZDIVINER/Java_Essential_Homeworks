a45fc5334991b92495abec8315ba10cb792fd0d1
Eugen <ezdiviner@gmail.com>
2022-08-17 15:46:40 +0300

    Finished Task5

---
+++ Homework2_3/src/Homework2_3_Task5/Main.java
@@ -0,0 +1,8 @@
+package Homework2_3_Task5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Площадь круга = " + MyArea.areaOfCircle(4));;
+
+    }
+}
