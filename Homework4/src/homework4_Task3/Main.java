89689db83c0c395e3350d268affebbcd5bf7fecf
Eugen <ezdiviner@gmail.com>
2022-08-28 20:40:50 +0300

    Finished Task3

---
+++ Homework4/src/homework4_Task3/Main.java
@@ -0,0 +1,11 @@
+package homework4_Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play();
+        player.pause();
+        player.record();
+        player.stop();
+    }
+}
---
+++ Homework4/src/homework4_Task3/Player.java
@@ -0,0 +1,24 @@
+package homework4_Task3;
+
+public class Player implements Playable, Recodable {
+    @Override
+    public void play() {
+        System.out.println("play");
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("stop");
+    }
+
+    @Override
+    public void record() {
+        System.out.println("record");
+    }
+}
+
